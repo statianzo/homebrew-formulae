37093269fe99c1c17893e519380f69a0aa39bb3e
Jason Staten <code.github@jxs.me>
2025-05-05 16:42:40 -0600

    final description fix

--- hyper-mcp.rb
+++ hyper-mcp.rb
@@ -1,5 +1,5 @@
 class HyperMcp < Formula
-  desc "MCP server that extends its capabilities through WebAssembly plugins."
+  desc "MCP server that extends its capabilities through WebAssembly plugins"
   homepage "https://github.com/tuananh/hyper-mcp"
   url "https://github.com/tuananh/hyper-mcp/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "b628478ff47a79bccc36864f301a489540f10bd5e4794723d6435a2d8e74bb3e"
